22fd3f6c764d484df571b977ca23d7c77a2d9edc
Ajsa Besirovic <besirovicajsa@gmail.com>
2022-09-05 19:16:02 +0200

    trying out get method at different paths

--- main.py
+++ main.py
@@ -5,9 +5,18 @@ app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"message": "Hello World AJSAAA"}
+    return {"message": "Hello World"}
 
 
-@app.get("/about")
-def about():
-    return {'about': "this is about"}
+@app.get('/blog/unpublished')
+def unpublished () :
+    return {'data':'all unpublished blogs'}
+
+@app.get("/blog/{id}")
+def about(id: int):
+    return {'data': id}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id):
+    return {'data': {'1','2'}}
