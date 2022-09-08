501fc27d29d849aa98723ef923a90087d63b777f
Ajsa Besirovic <besirovicajsa@gmail.com>
2022-09-08 12:10:09 +0200

    creating model and table

--- blog/database.py
+++ blog/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'sqlite:///.blog.db'
+DATABASE_URL = 'sqlite:///./blog.db'
 engine = create_engine(DATABASE_URL,connect_args={'check_same_thread':False})
 
+SessionLocal = sessionmaker( bind=engine, autocommit=False, autoflush=False,)
 Base = declarative_base()
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- blog/main.py
+++ blog/main.py
@@ -1,29 +1,21 @@
-from pydantic import BaseModel
-from fastapi import FastAPI
-from . import schemas
-from enum import Enum
-
-class ModelName(str, Enum):
-    alexnet = "ajsa"
-    resnet = "resnet"
-    lenet = "lenet"
-
+from this import d
+from fastapi import FastAPI,Depends
+from .database import engine,SessionLocal
+from .  import schemas, models
+from sqlalchemy.orm import Session
 
 app = FastAPI()
+models.Base.metadata.create_all(engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-# @app.get("/models/{model_name}")
-# async def get_model(model_name: ModelName):
-#     if model_name is ModelName.alexnet:
-#         return {'modelname':model_name, 'value':model_name.value}
-#         return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-#     if model_name.value == "lenet":
-#         return {"model_name": model_name, "message": "LeCNN all the images"}
-
-#     return {"model_name": model_name, "message": "Have some residuals"}
 
 @app.post('/blog')
-def create(request: schemas.Blog):
-    return request
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    return db
 
