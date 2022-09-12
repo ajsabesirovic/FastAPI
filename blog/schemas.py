3c63c74322b27fceb6f49028c49496676ca482ac
Ajsa Besirovic <besirovicajsa@gmail.com>
2022-09-12 20:56:52 +0200

    creating the relationship and adding the foreign key

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,12 +1,11 @@
 from pydantic import BaseModel
+from typing import List
 
-class Blog(BaseModel):
+class BlogBase(BaseModel):
     title: str
-    body: str
+    body: str    
 
-class ShowBlog(BaseModel):
-    title: str
-    body: str
+class Blog(BlogBase):
     class Config():
         orm_mode = True
 
@@ -15,9 +14,16 @@ class User(BaseModel):
     email: str
     password: str
 
-
 class ShowUser(BaseModel):
     name: str
     email: str
+    blogs: List[Blog] = []
+    class Config():
+        orm_mode = True
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    creator: ShowUser
     class Config():
         orm_mode = True
